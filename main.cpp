999bfeda13220317753ac004ed4328bdb33d01cb
aidan <aidanboudreau@ufl.edu>
2026-03-06 01:55:42 -0500

    added LRU

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 #include <vector>
+#include <algorithm>
 
 int main()
 {
@@ -24,6 +25,8 @@ int main()
         cout << r[i] << " ";
     }
     cout<<endl;
+
+
     cout << "FIFO: " << endl;
 
     vector<int> blockVect(k, NULL);
@@ -35,21 +38,26 @@ int main()
         cout<<"iteration " << i+1 << endl;
         for (int j = 0; j < blockVect.size(); j++){
             if (blockVect[j]==NULL){
+                //empty cache
                 blockVect[j] = r[i];
                 missCounter++;
                 accessVect.push_back(blockVect[j]);
                 break;
             }
             if (blockVect[j]==r[i]){
+                //cache hit
                 hitCounter++;
                 break;
             }
             if (j==blockVect.size()-1){
+                //request is not in cache
                 for (int k = 0; k < blockVect.size(); k++){
+                    //find the cache block that has first in cache, replace it with the request
                     if (blockVect[k] == accessVect[0]){
                         blockVect[k]=r[i];
                     }
                 }
+                //take out first item from FIFO list, make the request next in queue
                 accessVect.erase(accessVect.begin());
                 accessVect.push_back(r[i]);
                 missCounter++;
@@ -61,4 +69,57 @@ int main()
     }
     cout << "number of FIFO misses: " << missCounter <<endl;
     cout << "number of FIFO hits: " << hitCounter <<endl;
+
+
+    
+
+
+
+    cout << "LRU: " << endl;
+
+    vector<int> blockVect2(k, NULL);
+    blockVect=blockVect2;
+    missCounter = 0;
+    hitCounter = 0;
+    accessVect.clear();
+
+    for (int i = 0; i < r.size(); i++){
+        cout<<"iteration " << i+1 << endl;
+        for (int j = 0; j < blockVect.size(); j++){
+            if (blockVect[j]==NULL){
+                //empty cache
+                blockVect[j] = r[i];
+                missCounter++;
+                accessVect.push_back(blockVect[j]);
+                break;
+            }
+            if (blockVect[j]==r[i]){
+                //cache hit
+                //remove request from LRU vector and place it on the end
+                accessVect.erase(std::remove(accessVect.begin(), accessVect.end(), r[i]), accessVect.end());
+                accessVect.push_back(r[i]);
+                hitCounter++;
+                break;
+            }
+            if (j==blockVect.size()-1){
+                //request is not in cache
+                for (int k = 0; k < blockVect.size(); k++){
+                    //find the cache block that has first in cache, replace it with the request
+                    if (blockVect[k] == accessVect[0]){
+                        blockVect[k]=r[i];
+                    }
+                }
+                //take out first item from FIFO list, make the request next in queue
+                accessVect.erase(accessVect.begin());
+                accessVect.push_back(r[i]);
+                missCounter++;
+            }
+        }
+        for (int i = 0; i < blockVect.size(); i++) {
+            cout << blockVect[i] << endl;
+        }
+    }
+    cout << "number of LRU misses: " << missCounter <<endl;
+    cout << "number of LRU hits: " << hitCounter <<endl;
+
 }
